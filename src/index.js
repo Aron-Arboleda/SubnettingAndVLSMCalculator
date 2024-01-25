6110d3c9c6f1ce388116609830049bd17bc7d8b7
Aron-Arboleda <aronrez62@gmail.com>
2024-01-26 00:09:53 +0800

    Media Styles DONE!

--- src/index.js
+++ src/index.js
@@ -7,6 +7,29 @@ const siteLogo = document.querySelector('header img');
 const contentArea = document.querySelector('main div');
 const sidebarsBtn = document.querySelectorAll('aside button');
 
+const menuBtn = document.querySelector('nav #nav-button');
+const sidebar = document.querySelector('aside');
+
+function windowOnClick(event) {
+    if (!sidebar.contains(event.target) && !menuBtn.contains(event.target)) {
+      sidebar.style.display = 'none';
+      window.removeEventListener('click', windowOnClick);
+    }
+  }
+  
+menuBtn.onclick = () => {
+    sidebar.style.display = 'flex';
+    window.addEventListener('click', windowOnClick);
+};
+
+window.addEventListener('resize', function() {
+    if (window.innerWidth >= 956){
+        sidebar.style.display = 'flex';
+    } else {
+        sidebar.style.display = 'none';
+    }
+});
+
 export function unchild(parent) {
     if (parent) {
       let child = parent.firstChild;
